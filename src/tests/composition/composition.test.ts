434f09bdb696c5b4bbfd1ddd765130a451b15aec
Alejandro Leal <22180346+AstroCorp@users.noreply.github.com>
2025-04-14 15:06:33 +0200

    new test for composition

--- src/tests/composition/composition.test.ts
+++ src/tests/composition/composition.test.ts
@@ -1,10 +1,21 @@
 import Composition from "./classes/Composition";
+import CompositionWithConflicts from "./classes/CompositionWithConflicts";
 
 describe('Trait Composition', () => {
-    test('should maintain functionality of all combined traits', () => {
+    test('should allow creating a trait by combining other traits', () => {
         const composition = Composition.create();
+        
         expect(composition.sum(1, 2)).toBe(3);
         expect(composition.sub(1, 2)).toBe(-1);
         expect(composition.log('Hello')).toEqual('Hello');
+    });
+
+    test('should maintain functionality of all combined traits', () => {
+        const composition = CompositionWithConflicts.create();
+
+        expect(composition.sum(1, 2)).toBe(3);
+        expect(composition.sub(1, 2)).toBe(-1);
+        expect(composition.log('Hello')).toEqual('Hello');
+        expect(composition.debug()).toEqual('CompositionDebugger debug');
     });
 });
---
+++ src/tests/composition/traits/CompositionDebugger.ts
@@ -0,0 +1,11 @@
+class CompositionDebugger {
+    log() {
+        return 'CompositionDebugger log';
+    }
+
+    debug() {
+        return 'CompositionDebugger debug';
+    }
+}
+
+export default CompositionDebugger;
