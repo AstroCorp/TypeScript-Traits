c99e1e1bb6e7c16f09578b7a0d32e99f30777b78
Alejandro Leal <22180346+AstroCorp@users.noreply.github.com>
2025-05-15 14:51:59 +0200

    remove properties tests

--- src/tests/properties/types/types.d.ts
+++
@@ -1,7 +0,0 @@
-import Properties from "../classes/Properties";
-import PropertiesWithError from "../classes/PropertiesWIthError";
-import PropertiesFile from "../traits/PropertiesFile";
-
-export type PropertiesInstanceType = Properties & PropertiesFile;
-
-export type PropertiesWithErrorInstanceType = PropertiesWithError & PropertiesFile;
